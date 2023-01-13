ab8adc99336801e134983b0faab024b3235995fb
Vadim <vadim.bogaiskov@gmail.com>
2023-01-13 15:51:32 +0300

    Added GitHub Plugin URI

--- bg_pbu.php
+++ bg_pbu.php
@@ -7,6 +7,7 @@
     Author: VBog
     Author URI: https://bogaiskov.ru 
 	License:     GPL2
+	GitHub Plugin URI: https://github.com/VBog/bg-patriarchia-bu
 */
 
 /*  Copyright 2018-2023  Vadim Bogaiskov  (email: vadim.bogaiskov@gmail.com)
